50cbf568b1a3d2ea0eb35b6db7ee4b48b3115705
Jhonatan Pacheco <jhonatan.tec@gmail.com>
2024-10-09 07:07:03 -0300

    Atualizado: atualizando configuração

--- src/models/dev.js
+++ src/models/dev.js
@@ -1,11 +1,16 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import PointSchema from "../utils/PointSchema.js";
 
-const DevSchema = new mongoose({
+const DevSchema = new Schema({
   name: String,
   github_username: String,
   bio: String,
   avatar_url: String,
   techs: [String],
+  location: {
+    type: PointSchema,
+    index: "2dsphere",
+  },
 });
 
 const devmap = mongoose.model("Dev", DevSchema);
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,29 @@
 import { Router } from "express";
 import axios from "axios";
-import devmap from "./models/dev";
+import Dev from "./models/dev.js";
 
 const routes = new Router();
 
 routes.post("/devs", async (req, res) => {
-  const { github_username } = req.body;
+  const { github_username, techs } = req.body;
 
   const response = await axios.get(
     `https://api.github.com/users/${github_username}`
   );
+
+  const { name = login, avatar_url, bio } = response.data;
+
+  const techsArray = techs.split(",").map((tech) => tech.trim());
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray,
+  });
+
+  return res.json(dev);
 });
 
 export default routes;
